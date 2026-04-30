5ca3c6130461c725041971cd067e59acefe73f66
abdullahee297 <232841637+abdullahee297@users.noreply.github.com>
2026-04-30 11:19:39 +0500

    index.py udated

--- api/index.py
+++ api/index.py
@@ -1,3 +1,6 @@
-from weatherproject.wsgi import application
+import os
+from django.core.wsgi import get_wsgi_application
 
-app = application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weatherproject.settings")
+
+app = get_wsgi_application()
